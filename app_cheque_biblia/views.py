825e6b26bbcffc8c2348375c783ed88d86c82fdc
moisesmlx <59755459+moisesmlx@users.noreply.github.com>
2024-02-20 10:49:54 -0300

    Update views.py

--- app_cheque_biblia/views.py
+++ app_cheque_biblia/views.py
@@ -10,12 +10,14 @@ from django.shortcuts import render
 import Cheque
 from django.http import HttpResponse
 
-def index(request):  
+def index(request):
     return render(request, 'index.html')
 
 def ver_pesquisa(request):
+    palavra = request.POST.get('p')
+
     try:
-        palavra = request.POST.get('p')
+
         if palavra != '':
             Cheque.Cheque_Palavras(palavra)
             text_area = Cheque.checagem
@@ -28,12 +30,14 @@ def ver_pesquisa(request):
             }
             return render(request, 'ver_pesquisa.html', lista)
         else:
-            return HttpResponse('''<h1>
-                                <center>Cheque se digitou uma palavra ou frase que tenha em nossa tradução; 
-                                tente outras palavras com o mesmo significado!<br>
+            return HttpResponse(f'''<h1>
+                                        <center>Não foi encontrada a palavra {palavra}.
+                                        Cheque, se escreveu a palavra corretamente, ou tente uma outra palavra com o mesmo significado.<br>
+                                        Lembre_se escrever com letra maiúscula quando for nomes próprio, senão escreva com letras minúsculas, e de usar a acentuação corretamente ok!<br>
+                                        Lembrando que a Tradução da biblia do site e de João Ferreira de Almeida Revista e atualizada.<br>
+                                        Exemplo: Salmo 1 verso 1 -> nesta tradução começa com bem aventurado e não com feliz,<br>
+                                        Fica mais fácil se procurar palavras chaves como: fé, amor, socorro, Daví, Eliseu...<br>
                                 <a href="javascript: history.go(-1)">Voltar</a>
                                 </center></h1>''')
-    except:
-        return render(request, 'ver_pesquisa.html', lista)
-
-    
+    except Exception as error:
+        return HttpResponse(f'Não foi possivel checar a palavra ou frase "{palavra}"\n O erro foi{error}')
